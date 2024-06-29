c730d68dffe9209c7816dc71297cbd6192f3233e
TVNL CHARAN <vnlcharan.t@gmail.com>
2024-06-29 16:10:02 +0530

    testing login auth

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,42 +1,57 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 import "./Login.css";
-import { jwtDecode } from "jwt-decode";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function Login() {
   const navigate = useNavigate();
 
   function handleCallbackResponse(response) {
-    const email = jwtDecode(response.credential).email;
-    const emails = [
-      "n200232@rguktn.ac.in",
-      "n200086@rguktn.ac.in",
-      "sivahere9484@gmail.com",
-      "vnlcharan.t@gmail.com",
-    ];
-    if (emails.includes(email)) {
-      localStorage.setItem("user", JSON.stringify(response.credential));
-      navigate("/home");
-    }
+    const token = response.credential;
+
+    axios
+      .get("https://course-mate-server.onrender.com/user/login", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => {
+        if (res.status === 200) {
+          localStorage.setItem("user", JSON.stringify(token));
+          navigate("/home");
+        }
+      })
+      .catch((error) => {
+        if (error.response && error.response.status === 401) {
+          toast.error("Please login with your college Mail ID");
+        } else {
+          console.error("Login error:", error);
+          toast.error("An error occurred during login. Please try again.");
+        }
+      });
   }
 
   useEffect(() => {
     /* global google */
-    // google.accounts.id.initialize({
-    //   client_id: '886564988858-7ndnqcjaht1j2oddkcmdj8drlfkprmkf.apps.googleusercontent.com',
-    //   callback: handleCallbackResponse
-    // });
+    google.accounts.id.initialize({
+      client_id:
+        "886564988858-7ndnqcjaht1j2oddkcmdj8drlfkprmkf.apps.googleusercontent.com",
+      callback: handleCallbackResponse,
+    });
 
     google.accounts.id.renderButton(document.getElementById("signInDiv"), {
       theme: "outline",
       size: "large",
     });
 
-    // google.accounts.id.prompt();
+    google.accounts.id.prompt();
   }, [navigate]);
 
   return (
     <div className="login-container">
+      <ToastContainer />
       <div className="login-content row">
         <h1 className="display-3 fw-bold text-center cust-text">
           Welcome To CoursMate
@@ -44,10 +59,8 @@ function Login() {
         <div className="login-left justify-content-center col-12 col-sm-12 col-md-6">
           <img src="/coursemate.jpeg" height={"200px"} alt="Coursemate" />
         </div>
-        <div className="login-right justify-content-center  col-12 col-sm-12 col-md-6">
-          <div id="signInDiv" onClick={() => navigate("/home")}>
-            <button className="btn btn-primary">Login</button>
-          </div>
+        <div className="login-right justify-content-center col-12 col-sm-12 col-md-6">
+          <div id="signInDiv"></div>
         </div>
       </div>
     </div>
