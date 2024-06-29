75c8d71b866b18b9ea27ea13febda0b602daff5a
siva <sivahere9484@gmail.com>
2024-06-29 20:52:44 +0530

    sound delay solved

--- src/components/SoundHook/useClickSound.jsx
+++ src/components/SoundHook/useClickSound.jsx
@@ -3,11 +3,13 @@ import { useEffect } from 'react';
 const useClickSound = (soundFile, excludedSelectors = []) => {
   useEffect(() => {
     const clickSound = new Audio(soundFile);
+    clickSound.preload = 'auto'; // Ensure audio file is preloaded
 
     const handleClick = (event) => {
       if (excludedSelectors.some(selector => event.target.closest(selector))) {
         return;
       }
+      clickSound.currentTime = 0; // Rewind sound to start
       clickSound.play();
     };
 
